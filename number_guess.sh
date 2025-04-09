e42d8474dae5800f76ec3911bf9908df052dc6ee
Monyvann <102617852+MonyVannn@users.noreply.github.com>
2025-04-09 14:59:07 -0500

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -6,7 +6,6 @@ INPUT_NAME() {
   read NAME
   n=${#NAME}
 
-  #When you run your script, you should prompt the user for a username with Enter your username:, and take a username as input.Your database should allow usernames that are 22 characters
   if [[ ! $n -le 22 ]] || [[ ! $n -gt 0 ]]
   then
     INPUT_NAME
@@ -14,23 +13,16 @@ INPUT_NAME() {
     USER_NAME=$(echo $($PSQL "SELECT username FROM users WHERE username='$NAME';") | sed 's/ //g')
     if [[ ! -z $USER_NAME ]]
     then
-      #If that username has been used before, it should print Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses., 
-      #with <username> being a users name from the database,
-      #<games_played> being the total number of games that user has played, 
-      #and <best_game> being the fewest number of guesses it took that user to win the game
       USER_ID=$(echo $($PSQL "SELECT user_id FROM users WHERE username='$USER_NAME';") | sed 's/ //g')
       USER_NAME=$(echo $($PSQL "SELECT username FROM users WHERE user_id='$USER_ID';") | sed 's/ //g')
       GAME_PLAYED=$(echo $($PSQL "SELECT frequent_games FROM users WHERE user_id=$USER_ID;") | sed 's/ //g')
       BEST_GAME=$(echo $($PSQL "SELECT MIN(best_guess) FROM users LEFT JOIN games USING(user_id) WHERE user_id=$USER_ID;") | sed 's/ //g')
       echo "Welcome back, $USER_NAME! You have played $GAME_PLAYED games, and your best game took $BEST_GAME guesses."
     else
-      #If the username has not been used before, you should print Welcome, <username>! It looks like this is your first time here.
       USER_NAME=$NAME
       echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
     fi
 
-    #Your script should randomly generate a number that users have to guess
-    #The next line printed should be Guess the secret number between 1 and 1000: and input from the user should be read
     CORRECT_ANSWER=$(( $RANDOM % 1000 + 1 ))
     GUESS_COUNT=0
     INPUT_GUESS $USER_NAME $CORRECT_ANSWER $GUESS_COUNT
@@ -48,7 +40,6 @@ INPUT_GUESS() {
     echo "Guess the secret number between 1 and 1000:"
     read USSER_GUESS
   else
-    #If anything other than an integer is input as a guess, it should print That is not an integer, guess again:
     echo "That is not an integer, guess again:"
     read USSER_GUESS
   fi
@@ -62,17 +53,13 @@ INPUT_GUESS() {
   fi
 }
 
+
 CHECK_ANSWER() {
   USER_NAME=$1 
   CORRECT_ANSWER=$2 
   GUESS_COUNT=$3
   USSER_GUESS=$4
   
-  #Until they guess the secret number, 
-  #it should print 
-  #It's lower than that, guess again: if the previous input was higher than the secret number, and
-  #It's higher than that, guess again: if the previous input was lower than the secret number. 
-  #Asking for input each time until they input the secret number.
   if [[ $USSER_GUESS -lt $CORRECT_ANSWER ]]
   then
     echo "It's lower than that, guess again:"
@@ -94,8 +81,6 @@ CHECK_ANSWER() {
     CHECK_ANSWER $USER_NAME $CORRECT_ANSWER $GUESS_COUNT $USSER_GUESS
   elif [[ $USSER_GUESS -eq $CORRECT_ANSWER ]]
   then
-    #When the secret number is guessed, your script should print You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. 
-    #Nice job! and finish running
     SAVE_USER $USER_NAME $GUESS_COUNT
     NUMBER_OF_GUESSES=$GUESS_COUNT
     SECRET_NUMBER=$CORRECT_ANSWER
@@ -103,6 +88,7 @@ CHECK_ANSWER() {
   fi
 
 }
+
 
 SAVE_USER() {
   USER_NAME=$1 
@@ -119,6 +105,8 @@ SAVE_USER() {
   SAVE_GAME $USER_NAME $GUESS_COUNT
 }
 
+
+
 SAVE_GAME() {
   USER_NAME=$1 
   NUMBER_OF_GUESSES=$2
@@ -127,6 +115,7 @@ SAVE_GAME() {
   INSERT_GAME=$($PSQL "INSERT INTO games(user_id, best_guess) VALUES($USER_ID, $NUMBER_OF_GUESSES);")
   USER_NAME=$($PSQL "SELECT username FROM users WHERE user_id=$USER_ID;")
 }
+
 
 
 INPUT_NAME
